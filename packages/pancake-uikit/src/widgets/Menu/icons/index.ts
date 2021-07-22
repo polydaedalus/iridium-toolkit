a9c472b438d4d821b729516bc30e40cb5141c670
PlatinumFinance <86154134+polydaedalus@users.noreply.github.com>
2021-07-23 01:22:29 +0300

    Added more icons

--- packages/pancake-uikit/src/widgets/Menu/icons/index.ts
+++ packages/pancake-uikit/src/widgets/Menu/icons/index.ts
@@ -17,3 +17,5 @@ export { default as TelegramIcon } from "./Telegram";
 export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
+export { default as LayerIcon } from "./Layer";
+export { default as VaultIcon } from "./Vaults";
